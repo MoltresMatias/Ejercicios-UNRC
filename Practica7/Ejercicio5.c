2f4e6f820b0fb50767d1ab18d7580210ed0f3ca6
Matias <matiaspupirivarola@gmail.com>
2025-06-09 19:37:41 -0300

    Ejercicio5 de la pratica7

---
+++ Practica7/Ejercicio5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define max 200 
+typedef struct 
+{
+    int a[max];
+    int cant;
+}Tdata;
+
+Tdata misNotas;
+float promedio;
+
+void cargarNotas(Tdata *notas);
+void promedioNotas(Tdata a, float *b);
+
+int main(){
+    cargarNotas(&misNotas);
+    promedioNotas(misNotas, &promedio);
+    printf("El promedio final es: %.2f", promedio);
+    return 0;
+}
+
+void cargarNotas(Tdata *notas){
+    int i;
+    printf("Ingrese la cantidad de notas a cargar: \n");
+    scanf("%d", &notas->cant);
+    for (i = 1; i <= notas->cant; i++){
+        printf("Ingrese la nota: \n");
+        scanf("%d", &notas->a[i]);
+    }
+}
+
+void promedioNotas(Tdata a, float *b){
+    int i;
+    float sum;
+    sum = 0;
+    i = 1;
+    while (i <= a.cant)
+    {
+        sum = sum + a.a[i];
+        i++;
+    }
+    *b = sum/a.cant;
+}
